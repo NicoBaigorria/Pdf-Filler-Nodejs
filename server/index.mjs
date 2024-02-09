5a9b421d61d5ff418760fefa6d825314b81c291b
Nicolás Baigorria <nicobaig@hotmail.com>
2024-02-09 16:07:29 -0300

    creacion de pdf exitosa

--- server/index.mjs
+++ server/index.mjs
@@ -1,7 +1,7 @@
 import express from "express";
-import fs from 'fs';
-import path from 'path';
-import * as pdfjs from 'pdfjs-dist/build/pdf.min.mjs'
+import fs from "fs";
+import path from "path";
+import * as pdfjs from "pdfjs-dist/build/pdf.min.mjs";
 
 const app = express();
 const port = 3000;
@@ -32,21 +32,58 @@ function buscarPropiedad(json, targetName) {
 }
 
 // Set the path to the PDF file
-const pdfUrl = 'https://21669225.fs1.hubspotusercontent-na1.net/hubfs/21669225/PDF-Gobierno_de_Canada/imm1294e.pdf';
+const pdfUrl =
+  "https://21669225.fs1.hubspotusercontent-na1.net/hubfs/21669225/PDF-Gobierno_de_Canada/imm1294e.pdf";
 
-
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
     // Asynchronous download of PDF
     const pdf = await pdfjs.getDocument({ url: pdfUrl, enableXfa: true });
-    const pdfData = await pdf.saveDocument();
 
-    console.log('PDF saved:');
+    pdf.promise.then(function (pdfdata) {
+      console.log("PDF loaded");
 
-    res.send('PDF generated and saved successfully!');
+      const xfa = pdfdata.allXfaHtml;
+
+      console.log(xfa);
+
+      // Uso de la función
+      const resultadoSelect = buscarPropiedad(xfa, "select");
+      console.log("Resultado para 'select':", resultadoSelect);
+
+      const resultadoInput = buscarPropiedad(xfa, "input");
+      console.log("Resultado para 'input':", resultadoInput);
+
+      const resultadotextarea = buscarPropiedad(xfa, "textarea");
+      console.log("Resultado para 'textarea':", resultadotextarea);
+
+      pdfdata.annotationStorage.setValue("FamilyName31585", {
+        value: "asdsadas",
+      });
+
+      pdfdata.annotationStorage.setValue("Sex31593", { value: "Male" });
+
+      //pdfdata.getData().then(res =>{
+      pdfdata.saveDocument().then((newpdf) => {
+        console.log(newpdf);
+        
+        // Write to file
+        const outputPath = path.join("./output.pdf");
+        fs.writeFile(outputPath, Buffer.from(newpdf), (err) => {
+          if (err) {
+            console.error("Error writing PDF:", err);
+            res.status(500).send("Error writing PDF");
+            return;
+          }
+          console.log("PDF saved successfully!");
+        });
+
+        res.send("PDF generated and saved successfully!");
+      });
+    });
   } catch (error) {
-    console.error('Error generating PDF:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error generating PDF:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
