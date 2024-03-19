a3056ac57e828fb243a64b56df2776f1baab7645
Nicolás Baigorria <nicobaig@hotmail.com>
2024-03-19 15:44:54 -0300

    solucionado error campos
    tener cuidado con el tipo de dato

--- server/src/controlles/pdf-filler.mjs
+++ server/src/controlles/pdf-filler.mjs
@@ -5,41 +5,14 @@
 import fs from "fs";
 import path from "path";
 import * as pdfjs from "pdfjs-dist/build/pdf.min.mjs";
-import { getTicket, createFolder, createFile, deleteFolder, updateProperty } from "../services/hubspot.mjs";
+import {
+  getTicket,
+  createFolder,
+  createFile,
+  deleteFolder,
+  updateProperty,
+} from "../services/hubspot.mjs";
 import internal from "stream";
-
-/**
- * Mapea el xfa y devuelve la lista de campos que coincidan con el tipo del segundo parametro.
- *
- * @param {Object} json - The JSON object to search.
- * @param {string} targetName - Tipo de campo.
- * @returns {Array|null} - Lista de campos
- * .
- */
-function buscarPropiedad(json, targetName) {
-  const resultados = [];
-
-  /**
-   * Fucion para buscar campos.
-   *
-   * @param {Object} elemento - Xfa para revisar.
-   */
-  function buscarRecursivamente(elemento) {
-    if (elemento.name === targetName) {
-      resultados.push(elemento.attributes.dataId);
-    }
-
-    if (elemento.children && elemento.children.length > 0) {
-      for (const child of elemento.children) {
-        buscarRecursivamente(child);
-      }
-    }
-  }
-
-  buscarRecursivamente(json);
-
-  return resultados.length > 0 ? resultados : null;
-}
 
 /**
  * Procesar lista de campos busca su equivalente en hubspot y lo rellena.
@@ -47,102 +20,103 @@ function buscarPropiedad(json, targetName) {
  * @param {Object} objeto - Lista de campos que se va a procesar.
  * @param {Object} pdfdata - La información del PDF.
  * @param {Object} matchPropiedades - Las propiedades de coincidencia para buscar.
- * @param {Object} tickeProperties - Las propiedades del ticket para obtener los valores.
+ * @param {Object} ticketProperties - Las propiedades del ticket para obtener los valores.
  * @returns {Promise<void>} - Una promesa que se resuelve cuando se completa el procesamiento.
  */
-async function procesarCampo(objeto, pdfdata, matchPropiedades, tickeProperties, logs) {
-
+async function procesarCampo(
+  pdfdata,
+  matchPropiedades,
+  ticketProperties,
+  logs
+) {
   for (let tipo in matchPropiedades) {
     for (let campo in matchPropiedades[tipo]) {
-
       const InternalName = matchPropiedades[tipo][campo].hubspotProperty;
-      const value = tickeProperties[InternalName];
-      //  console.log("familyname", tickeProperties.familyname)
-      if (logs) console.log("campo: " + campo + " llenado con " + value);
+      const value = ticketProperties[InternalName];
+      //  console.log("familyname", ticketProperties.familyname)
+      //  if (logs) console.log("campo: " + campo + " llenado con " + value);
       try {
-        if (campo == "FamilyName240550") console.log("encontradooooooo")
-        await pdfdata.annotationStorage.setValue(campo, { value: value });
+        if (value) await pdfdata.annotationStorage.setValue(campo, { value: value });
       } catch (e) {
-        console.log("Error al llenar campo: " + campo)
+        console.log("Error al llenar campo: " + campo);
       }
     }
   }
 }
-
 
 /**
  * Procesar pdf para rellenar.
  *
  * @param {string} pdfInput - Nombre del Pdf.
  * @param {string} folder - Url de salida del Pdf.
- * @param {Object} tickeProperties - Propiedades de Ticket.
+ * @param {Object} ticketProperties - Propiedades de Ticket.
  * @returns {Promise<void>} - Promesa de que el archivo sera escrito.
  */
-const procesarPdf = async (pdfInput, folder, tickeProperties) => {
+const procesarPdf = async (pdfInput, folder, ticketProperties) => {
   try {
-    const pdf = await pdfjs.getDocument({ url: "./src/InputFiles/" + pdfInput, enableXfa: true });
+    const pdf = await pdfjs.getDocument({
+      url: "./src/InputFiles/" + pdfInput,
+      enableXfa: true,
+    });
 
     await pdf.promise.then(async function (pdfdata) {
       console.log("PDF loaded: " + pdfInput);
 
-      const xfa = pdfdata.allXfaHtml;
+      const xfa = await pdfdata.allXfaHtml;
 
       if (xfa) {
-        // Extraer todos los campos para rellenar.
-        const resultadoSelect = buscarPropiedad(xfa, "select");
-        const resultadoInput = buscarPropiedad(xfa, "input");
-        const resultadotextarea = buscarPropiedad(xfa, "textarea");
-
-        //const matchPropiedades = await JSON.parse(fs.readFileSync("./src/Jsons/matchPropiedades.json", "utf8"));
-
         const fileNameWithoutExtension = path.parse(pdfInput).name;
 
+        console.log(
+          "qweqwe",
+          "./src/Jsons/matchPropsForms/" + fileNameWithoutExtension + ".json"
+        );
 
-        console.log("qweqwe", "./src/Jsons/matchPropsForms/" + fileNameWithoutExtension + ".json")
-
-        const matchPropiedades = await JSON.parse(fs.readFileSync("./src/Jsons/matchPropsForms/" + fileNameWithoutExtension + ".json", "utf8"));
+        const matchPropiedades = await JSON.parse(
+          fs.readFileSync(
+            "./src/Jsons/matchPropsForms/" + fileNameWithoutExtension + ".json",
+            "utf8"
+          )
+        );
 
         let logs = false;
 
         if (fileNameWithoutExtension == "imm1294e") logs = true;
 
-        // Array para almacenar las promesas de procesamiento de campos
-        const promises = [];
-
-        // Rellenar campos con datos de Hubpost.
-        promises.push(procesarCampo(resultadotextarea, pdfdata, matchPropiedades, tickeProperties, logs));
-        promises.push(procesarCampo(resultadoInput, pdfdata, matchPropiedades, tickeProperties, logs));
-        promises.push(procesarCampo(resultadoSelect, pdfdata, matchPropiedades, tickeProperties, logs));
-
         try {
           // Esperar a que todas las promesas se resuelvan
-          await Promise.all(promises);
-
-          // Guardar PDF.
-          const newpdf = await pdfdata.saveDocument();
-          const outputPath = path.join(folder, pdfInput);
-          await fs.promises.writeFile(outputPath, Buffer.from(newpdf));
-          console.log("PDF saved successfully!");
+          await procesarCampo(
+            pdfdata,
+            matchPropiedades,
+            ticketProperties,
+            logs
+          ).then(async () => {
+            // Guardar PDF.
+            if (fileNameWithoutExtension == "imm1294e") console.log("fghgfhgf", await pdfdata.annotationStorage.getAll());
+            const newpdf = await pdfdata.saveDocument();
+            const outputPath = path.join(folder, pdfInput);
+            await fs.promises.writeFile(outputPath, Buffer.from(newpdf));
+            console.log("PDF saved successfully!");
+          });
         } catch (error) {
           console.error("Error:", error);
         }
-
       } else {
         console.log("Not have xfa " + pdfInput);
 
         // Get the AcroForm (fillable form) fields
-        await pdfdata.getFieldObjects().then(async inputs => {
+        await pdfdata.getFieldObjects().then(async (inputs) => {
           for (let input in inputs) {
-
             if (inputs[input][0].type == "text") {
-              await pdfdata.annotationStorage.setValue(inputs[input][0].id, { value: "value" });
+              await pdfdata.annotationStorage.setValue(inputs[input][0].id, {
+                value: "value",
+              });
             }
           }
-        })
+        });
 
         // Guardar PDF.
         try {
-
           const newpdf = await pdfdata.saveDocument();
           const outputPath = path.join(folder, pdfInput);
           await fs.promises.writeFile(outputPath, Buffer.from(newpdf));
@@ -156,8 +130,7 @@ const procesarPdf = async (pdfInput, folder, tickeProperties) => {
       await pdfdata.destroy();
     });
   } catch (error) {
-    console.log("ERROR READING PDF: " + pdfInput)
-
+    console.log("ERROR READING PDF: " + pdfInput);
   }
 };
 
@@ -182,18 +155,19 @@ const postPdf = async (req, res) => {
     // Traer informacion del ticket para llenar formularios
 
     const ticketData = await getTicket(idTicket);
-    const tickeProperties = ticketData.properties;
+    const ticketProperties = ticketData.properties;
 
-    console.log(tickeProperties);
-
+    console.log(ticketProperties);
 
     // Selecciono los Formularios segun el plan
 
-    const listaProgramas = await JSON.parse(fs.readFileSync("./src/Jsons/planesForm.json", "utf8"));
+    const listaProgramas = await JSON.parse(
+      fs.readFileSync("./src/Jsons/planesForm.json", "utf8")
+    );
 
-    const programa = tickeProperties.programa_formularios
+    const programa = ticketProperties.programa_formularios;
 
-    const files = await fs.promises.readdir('./src/InputFiles');
+    const files = await fs.promises.readdir("./src/InputFiles");
 
     if (listaProgramas.hasOwnProperty(programa)) {
       listaProgramas = listaProgramas.filter(function (elemento) {
@@ -208,16 +182,14 @@ const postPdf = async (req, res) => {
     //reemplazar files por listaProgramas
 
     for (const file of files) {
-      const processingPromise = procesarPdf(file, folder, tickeProperties);
+      const processingPromise = procesarPdf(file, folder, ticketProperties);
       processingPromises.push(processingPromise);
     }
 
     await Promise.all(processingPromises);
 
-
     // Borrar carpeta en Hubpsot si existe.
-    await deleteFolder(tickeProperties.id_folder);
-
+    await deleteFolder(ticketProperties.id_folder);
 
     // Crear una carpeta en Hubspot.
 
@@ -225,18 +197,16 @@ const postPdf = async (req, res) => {
     console.log(folderId);
 
     const jsonPropsTicket = {
-      "id_folder": folderId
+      id_folder: folderId,
     };
-
 
     // Actualizar propiedad folder_id del Ticket.
     await updateProperty(idTicket, jsonPropsTicket);
 
-
     // Subir los PDFs a la nueva carpeta.
     const pdfsListos = await fs.promises.readdir(folder);
 
-    const uploadPromises = pdfsListos.map(async file => {
+    const uploadPromises = pdfsListos.map(async (file) => {
       await createFile(folder, file, folderId);
     });
     await Promise.all(uploadPromises);
